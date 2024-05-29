4047410208b2ba663501e01ab6b7c81df03776d6
yul243 <yul243@ucsd.edu>
2024-05-28 22:47:58 -0700

    prototype

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ const projection = d3.geoAlbersUsa()
 
 const path = d3.geoPath().projection(projection);
 
-const color = d3.scaleQuantize([0, 100000], d3.schemeBlues[9]);
-
 d3.json("https://d3js.org/us-10m.v1.json").then(us => {
     svg.append("g")
         .attr("class", "states")
@@ -21,7 +19,6 @@ d3.json("https://d3js.org/us-10m.v1.json").then(us => {
         .data(topojson.feature(us, us.objects.states).features)
         .enter().append("path")
         .attr("d", path)
-        .attr("fill", d => color(Math.random() * 100000))
         .on("mouseover", function(event, d) {
             d3.select(this)
                 .transition()
@@ -42,15 +39,11 @@ d3.json("https://d3js.org/us-10m.v1.json").then(us => {
             d3.select(this)
                 .transition()
                 .duration(200)
-                .style("fill", d => color(Math.random() * 100000))
+                .style("fill", null)
                 .attr("transform", "scale(1)");
 
             d3.select("#tooltip").remove();
         });
-
-    svg.append("path")
-        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-        .attr("class", "state-borders")
-        .attr("d", path);
 });
+
 
